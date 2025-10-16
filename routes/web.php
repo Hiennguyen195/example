957d437eb590ba7a5b699cb8009e1eed7fedd93f
Hiennguyen195 <nguyenhien19052000@gmail.com>
2025-10-16 09:48:53 +0700

    create job page

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,16 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->simplePaginate(3);
-    return view('jobs', ['jobs' => $jobs]);
+    return view('jobs.index', ['jobs' => $jobs]);
 })->name('jobs_listing');
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('create_job');
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 })->name('job_details');
 
 Route::get('/contact', function () {
