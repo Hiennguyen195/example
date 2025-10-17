2542b4455a1a651cf08ffc46a826887ae4d11543
Hiennguyen195 <nguyenhien19052000@gmail.com>
2025-10-17 14:36:32 +0700

    add validation

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,1 +1,1 @@
-<a {{ $attributes->merge(['class' => 'rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500']) }}>{{ $slot }}</a>
+<a {{ $attributes->merge(['class' => 'rounded-md bg-indigo-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500']) }}>{{ $slot }}</a>
