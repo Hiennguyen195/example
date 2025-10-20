e39c34a31f845956f2827f14cd0d14a5e647475c
Hiennguyen195 <nguyenhien19052000@gmail.com>
2025-10-20 14:30:07 +0700

    redirect cancel btn

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -58,7 +58,7 @@
             </div>
 
             <div class="flex items-center gap-x-6">
-                <a href="/jobs/{{ $job->id }}"><button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button></a>
+                <a href="/jobs"><button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button></a>
                 <div>
                     <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                     Update
